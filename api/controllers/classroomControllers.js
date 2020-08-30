f02ce19e57cdff5a6805f538f07e2244eb815bd8
yogesh123098 <yogeshpant293@gmail.com>
2020-08-30 13:56:39 +0545

    minor fixes

--- api/controllers/classroomControllers.js
+++ api/controllers/classroomControllers.js
@@ -18,8 +18,8 @@ const User = require("../models/User");
 
 // middleware
 const {
-	createClassroomValidation,
-	updateClassroomValidation,
+  createClassroomValidation,
+  updateClassroomValidation,
 } = require("../middleware/validation");
 
 // utility functions
@@ -31,122 +31,122 @@ const { errorMessage, nonExistenceError } = require("../utils/errorMessages");
 // Classroom methods
 
 module.exports.classes_get = async (req, res) => {
-	try {
-		const classrooms = [];
-		// looping through each classroom doc
-		for await (const doc of Classroom.find()) {
-			classrooms.push({
-				//ids
-				_creatorId: doc._creatorId,
-				_classId: doc._id,
-				//classroom info
-				classCode: doc.classCode,
-				className: doc.className,
-				classDescription: doc.classDescription,
-				// members
-				classMembers: doc.classMembers,
-				classWorks: doc.classWorks,
-				// dates
-				createdAt: doc.createdAt,
-				updatedAt: doc.updatedAt,
-			});
-		}
+  try {
+    const classrooms = [];
+    // looping through each classroom doc
+    for await (const doc of Classroom.find()) {
+      classrooms.push({
+        //ids
+        _creatorId: doc._creatorId,
+        _classId: doc._id,
+        //classroom info
+        classCode: doc.classCode,
+        className: doc.className,
+        classDescription: doc.classDescription,
+        // members
+        classMembers: doc.classMembers,
+        classWorks: doc.classWorks,
+        // dates
+        createdAt: doc.createdAt,
+        updatedAt: doc.updatedAt,
+      });
+    }
 
-		return res.status(200).send(classrooms);
-	} catch (err) {
-		console.error(err);
-		return res.status(400).send(err);
-	}
+    return res.status(200).send(classrooms);
+  } catch (err) {
+    console.error(err);
+    return res.status(400).send(err);
+  }
 };
 
 module.exports.class_detail_get = async (req, res) => {
-	try {
-		const classroomFound = await Classroom.findOne({ _id: req.params.classId });
-		console.log(classroomFound);
-		if (!classroomFound) throw nonExistenceError("classroom");
+  try {
+    const classroomFound = await Classroom.findOne({ _id: req.params.classId });
+    console.log(classroomFound);
+    if (!classroomFound) throw nonExistenceError("classroom");
 
-		// check creator
-		const isCreator = req.user._id === classroomFound._creatorId;
-		// check member
-		const isMember = classroomFound.classMembers.acceptedMembers.some(
-			(doc) => doc._memberId === req.user._id
-		);
+    // check creator
+    const isCreator = req.user._id === classroomFound._creatorId;
+    // check member
+    const isMember = classroomFound.classMembers.acceptedMembers.some(
+      (doc) => doc._memberId === req.user._id
+    );
 
-		if (!(isCreator || isMember))
-			throw errorMessage(
-				401,
-				"Access-denied",
-				"To access this classroom you must either be a member or the creator"
-			);
+    if (!(isCreator || isMember))
+      throw errorMessage(
+        401,
+        "Access-denied",
+        "To access this classroom you must either be a member or the creator"
+      );
 
-		const classroom = {
-			_id: classroomFound._id,
-			createdAt: classroomFound.createdAt,
-			updated: classroomFound.updated,
-			updatedAt: classroomFound.updatedAt,
-			className: classroomFound.classroomName,
-			classDescription: classroomFound.classroomDescription,
-			classCode: classroomFound.classroomCode,
-			classMembers: classroomFound.classroomMembers.acceptedMembers,
-			classWorks: classroomFound.classWorks,
-		};
+    const classroom = {
+      _id: classroomFound._id,
+      createdAt: classroomFound.createdAt,
+      updated: classroomFound.updated,
+      updatedAt: classroomFound.updatedAt,
+      className: classroomFound.classroomName,
+      classDescription: classroomFound.classroomDescription,
+      classCode: classroomFound.classroomCode,
+      classMembers: classroomFound.classroomMembers.acceptedMembers,
+      classWorks: classroomFound.classWorks,
+    };
 
-		return res.status(200).send(classroom);
-	} catch (err) {
-		console.error(err);
-		return res.status(400).send(err);
-	}
+    return res.status(200).send(classroom);
+  } catch (err) {
+    console.error(err);
+    return res.status(400).send(err);
+  }
 };
 
 module.exports.user_classes_get = async (req, res) => {
-	try {
-		const userFound = await User.findById(req.params.userId);
-		if (!userFound)
-			throw {
-				error: {
-					status: 400,
-					type: "Non-existence",
-					message: "Requested user does not exist",
-				},
-			};
+  try {
+    const userFound = await User.findById(req.params.userId);
+    if (!userFound)
+      throw {
+        error: {
+          status: 400,
+          type: "Non-existence",
+          message: "Requested user does not exist",
+        },
+      };
 
-		const classesAttendingFound = await Classroom.find({
-			_id: { $in: userFound.classesAtending },
-		});
-		const classesTeachingFound = await Classroom.find({
-			_id: { $in: userFound.classesTeaching },
-		});
+    const classesAttendingFound = await Classroom.find({
+      _id: { $in: userFound.classesAtending },
+    });
+    const classesTeachingFound = await Classroom.find({
+      _id: { $in: userFound.classesTeaching },
+    });
 
-		const attending = [];
-		const teaching = [];
+    const attending = [];
+    const teaching = [];
 
-		for (let doc of classesAttendingFound) {
-			attending.push({
-				_creatorId: doc._creatorId,
-				_classId: doc._id,
-				classCode: doc.classCode,
-				className: doc.className,
-				classDescription: doc.classDescription,
-				classMembers: doc.classMembers,
-				classWorks: doc.classWorks,
-				createdAt: doc.createdAt,
-				updatedAt: doc.updatedAt,
-			});
-		}
+    for (let doc of classesAttendingFound) {
+      attending.push({
+        _creatorId: doc._creatorId,
+        _classId: doc._id,
+        classCode: doc.classCode,
+        className: doc.className,
+        classDescription: doc.classDescription,
+        classMembers: doc.classMembers,
+        classWorks: doc.classWorks,
+        createdAt: doc.createdAt,
+        updatedAt: doc.updatedAt,
+      });
+    }
 
-		// more info for teaching classes
-		for (let doc of classesTeachingFound) {
-			teaching.push(doc);
-		}
+    // more info for teaching classes
+    for (let doc of classesTeachingFound) {
+      teaching.push(doc);
+    }
 
-		return res.status(200).send({
-			classesAttending: attending,
-			classesTeaching: teaching,
-		});
-	} catch (err) {
-		console.log(err);
-		return res.status(400).send(err);
-	}
+    return res.status(200).send({
+      classesAttending: attending,
+      classesTeaching: teaching,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send(err);
+  }
 };
 
 /**
@@ -154,75 +154,76 @@ module.exports.user_classes_get = async (req, res) => {
  * POST body = {className*: , classDescription: , classSubject: , affiliatedInstitution: ,}
  */
 module.exports.create_class_post = async (req, res) => {
-	try {
-		// validate post body
-		const { error } = createClassroomValidation(req.body);
-		if (error)
-			throw {
-				error: {
-					type: "Req.body Validation error",
-					message: error.details[0].message,
-				},
-			};
+  try {
+    // validate post body
+    const { error } = createClassroomValidation(req.body);
+    if (error)
+      throw {
+        error: {
+          type: "Req.body Validation error",
+          message: error.details[0].message,
+        },
+      };
 
-		// check if req.user exists
-		const userFound = await User.findById(req.user._id);
-		if (!userFound) {
-			throw {
-				error: {
-					status: 404,
-					type: "Non-existence",
-					message: "Requesting user not found",
-				},
-			};
-		}
+    // check if req.user exists
+    const userFound = await User.findById(req.user._id);
+    if (!userFound) {
+      throw {
+        error: {
+          status: 404,
+          type: "Non-existence",
+          message: "Requesting user not found",
+        },
+      };
+    }
 
-		// check uniqueness of class code
-		let unique = false;
-		while (unique === false) {
-			let code = uniqueClassCode();
-			let classFound = await Classroom.findOne({ classCode: code });
-			if (classFound) unique = false;
-			else unique = true;
-		}
+    // check uniqueness of class code
+    let code;
+    let unique = false;
+    while (unique === false) {
+      code = uniqueClassCode();
+      let classFound = await Classroom.findOne({ classCode: code });
+      if (classFound) unique = false;
+      else unique = true;
+    }
 
-		let classAttributes = {};
-		// required fields created in backend
-		classAttributes["_creatorId"] = req.user._id;
-		classAttributes["classCode"] = uniqueClassCode();
+    let classAttributes = {};
+    // required fields created in backend
+    classAttributes["_creatorId"] = req.user._id;
+    classAttributes["classCode"] = code;
 
-		// since all fields are not required, only those requested are added
-		for (let key in req.body) {
-			classAttributes[key] = req.body[key];
-		}
+    // since all fields are not required, only those requested are added
+    for (let key in req.body) {
+      classAttributes[key] = req.body[key];
+    }
 
-		const classroom = new Classroom(classAttributes);
-		const savedClassroom = await classroom.save();
-		console.log(savedClassroom);
+    const classroom = new Classroom(classAttributes);
+    const savedClassroom = await classroom.save();
+    console.log(savedClassroom);
 
-		const updateUserInfo = await User.updateOne(
-			{ _id: req.user._id },
-			{
-				$push: { classesTeaching: savedClassroom._id },
-			}
-		);
+    const updateUserInfo = await User.updateOne(
+      { _id: req.user._id },
+      {
+        $push: { classesTeaching: savedClassroom._id },
+      }
+    );
 
-		return res.status(200).send({
-			success: {
-				status: 200,
-				message: "Classroom created",
-				classroomInfo: {
-					_id: savedClassroom._id,
-					className: savedClassroom.className,
-					classCode: savedClassroom.classCode,
-					creator: savedClassroom._creatorId,
-				},
-			},
-		});
-	} catch (err) {
-		console.error(err);
-		return res.status(400).send(err);
-	}
+    return res.status(200).send({
+      success: {
+        status: 200,
+        message: "Classroom created",
+        classroomInfo: {
+          _id: savedClassroom._id,
+          className: savedClassroom.className,
+          classCode: savedClassroom.classCode,
+          creator: savedClassroom._creatorId,
+        },
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(400).send(err);
+  }
 };
 
 /**
@@ -230,48 +231,48 @@ module.exports.create_class_post = async (req, res) => {
  * PATCH body: {_classId: className*: ,classDescription: ,classSubject: ,affiliatedInstitution: ,}
  */
 module.exports.update_class_patch = async (req, res) => {
-	try {
-		const { error } = updateClassroomValidation(req.body);
-		if (error)
-			throw {
-				error: {
-					type: "Req.body Validation error",
-					message: error.details[0].message,
-				},
-			};
+  try {
+    const { error } = updateClassroomValidation(req.body);
+    if (error)
+      throw {
+        error: {
+          type: "Req.body Validation error",
+          message: error.details[0].message,
+        },
+      };
 
-		const classroomFound = await Classroom.findOne({ _id: req.params.classId });
-		if (!classroomFound) throw nonExistenceError("classroom");
+    const classroomFound = await Classroom.findOne({ _id: req.params.classId });
+    if (!classroomFound) throw nonExistenceError("classroom");
 
-		const query = { $set: {} };
-		query.$set["updated"] = true;
-		query.$set["updatedAt"] = Date.now();
+    const query = { $set: {} };
+    query.$set["updated"] = true;
+    query.$set["updatedAt"] = Date.now();
 
-		for (let key in req.body) {
-			if (classroomFound[key] && classroomFound[key] != req.body[key])
-				query.$set[key] = req.body[key];
-		}
+    for (let key in req.body) {
+      if (classroomFound[key] && classroomFound[key] != req.body[key])
+        query.$set[key] = req.body[key];
+    }
 
-		await Classroom.updateOne({ _id: classroomFound._id }, query);
+    await Classroom.updateOne({ _id: classroomFound._id }, query);
 
-		return res.status(200).send({
-			Success: {
-				status: 200,
-				message: "Classroom successfully updated",
-				classroomInfo: {
-					classId: classroomFound._id,
-					creator: classroomFound._creatorId,
-				},
-			},
-		});
-	} catch (err) {
-		console.error(err);
-		return res.status(400).send(err);
-	}
+    return res.status(200).send({
+      Success: {
+        status: 200,
+        message: "Classroom successfully updated",
+        classroomInfo: {
+          classId: classroomFound._id,
+          creator: classroomFound._creatorId,
+        },
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(400).send(err);
+  }
 };
 
 module.exports.class_delete = async (req, res) => {
-	console.log("class_delete");
+  console.log("class_delete");
 };
 
 //
@@ -282,44 +283,44 @@ module.exports.class_delete = async (req, res) => {
  * @method GET
  */
 module.exports.classworks_get = async (req, res) => {
-	console.log("classwork_get");
+  console.log("classwork_get");
 };
 
 /** CREATE classwork
  * @method POST
  */
 module.exports.create_classwork_post = async (req, res) => {
-	console.log("create_classwork_post");
+  console.log("create_classwork_post");
 };
 
 /** Read Detail view of a classroom
  * @method GET
  */
 module.exports.classwork_detail_get = async (req, res) => {
-	console.log("classwork_detail_get");
+  console.log("classwork_detail_get");
 };
 
 /** UPDATE classwork
  * @method PATCH
  */
 module.exports.update_classwork_patch = async (req, res) => {
-	console.log("update_classwork_patch");
+  console.log("update_classwork_patch");
 };
 
 /** DELETE classwork
  * @method DELETE
  */
 module.exports.classwork_delete = async (req, res) => {
-	console.log("update_classwork_patch");
+  console.log("update_classwork_patch");
 };
 
 //
 //
 // Classwork submisssions
 module.exports.submit_classwork_post = async (req, res) => {
-	console.log("submit_classwork_post");
+  console.log("submit_classwork_post");
 };
 
 module.exports.update_classwork_submissioin_patch = async (req, res) => {
-	console.log("update_classwork_submissioin_patch");
+  console.log("update_classwork_submissioin_patch");
 };
--- api/routes/classroomRoutes.js
+++ api/routes/classroomRoutes.js
@@ -23,40 +23,40 @@ const { loggedInVerify } = require("../middleware/loggedInVerify");
 //*  TODO make a possible middleware to implement roles like teacher student so teacher gets more info
 
 classroomRouter.get(
-	"/",
-	loggedInVerify,
-	classroomController.classes_get
+  "/",
+  loggedInVerify,
+  classroomController.classes_get
 ); /** get all classes available  @method GET @endpoint `api/classrooms` */
 
 classroomRouter.get(
-	"/user/:userId",
-	loggedInVerify,
-	classroomController.user_classes_get
+  "/user/:userId",
+  loggedInVerify,
+  classroomController.user_classes_get
 ); /** get all classes available for certain user @method GET @endpoint `api/classroom/:userId`*/
 
 // CRUD classroom
 classroomRouter.post(
-	"/create",
-	loggedInVerify,
-	classroomController.create_class_post
+  "/create",
+  loggedInVerify,
+  classroomController.create_class_post
 ); /** CREATE classroom @method POST @endpoint `api/classroom/:classId/update`*/
 
 classroomRouter.get(
-	"/:classId",
-	loggedInVerify,
-	classroomController.class_detail_get
+  "/:classId",
+  loggedInVerify,
+  classroomController.class_detail_get
 ); /** get certain classroom detail @method GET @endpoint `api/classroom/:classId`*/
 
 classroomRouter.patch(
-	"/:classId/update",
-	loggedInVerify,
-	classroomController.update_class_patch
+  "/:classId/update",
+  loggedInVerify,
+  classroomController.update_class_patch
 ); /** UPDATE classroom @method PATCH @endpoint `api/classroom/:classId/update`*/
 
 classroomRouter.delete(
-	"/:classId/delete",
-	loggedInVerify,
-	classroomController.class_delete
+  "/:classId/delete",
+  loggedInVerify,
+  classroomController.class_delete
 ); /** delete classroom @method DELETE @endpoint `api/classroom/:classId/delete`*/
 
 //
@@ -64,29 +64,29 @@ classroomRouter.delete(
 // CLASSWORK methods
 
 classroomRouter.get(
-	"/:classId/classworks",
-	loggedInVerify,
-	classroomController.classworks_get
+  "/:classId/classworks",
+  loggedInVerify,
+  classroomController.classworks_get
 ); /** get classroom classworks @method GET @endpoint `api/classroom/:classId/classworks`*/
 
 // classwork CRUD
 
 classroomRouter.post(
-	"/:classId/classworks/create",
-	loggedInVerify,
-	classroomController.create_classwork_post
+  "/:classId/classworks/create",
+  loggedInVerify,
+  classroomController.create_classwork_post
 ); /** create classwork @method POST @endpoint `api/classroom/:classId/classworks/create`*/
 
 classroomRouter.get(
-	"/:classId/classworks/:classworkId",
-	loggedInVerify,
-	classroomController.classwork_detail_get
+  "/:classId/classworks/:classworkId",
+  loggedInVerify,
+  classroomController.classwork_detail_get
 ); /** get certain classrooms classwork @method GET @endpoint `api/classroom/:classId/classworks/:classworkId`*/
 
 classroomRouter.patch(
-	"/:classId/classworks/:classworkId/update",
-	loggedInVerify,
-	classroomController.update_classwork_patch
+  "/:classId/classworks/:classworkId/update",
+  loggedInVerify,
+  classroomController.update_classwork_patch
 ); /** update classwork @method PATCH @endpoint `api/classroom/:classId/classworks/:classworkId/update`*/
 
 // classroomRouter.delete(
@@ -100,15 +100,15 @@ classroomRouter.patch(
 // STUDENT SUBMITS
 
 classroomRouter.post(
-	"/:classId/classworks/:classworkId/submit",
-	loggedInVerify,
-	classroomController.submit_classwork_post
+  "/:classId/classworks/:classworkId/submit",
+  loggedInVerify,
+  classroomController.submit_classwork_post
 ); /** submit classwork @method GET @endpoint `api/classroom/:classId/classworks/:classId/submit`*/
 
 classroomRouter.patch(
-	"/:classId/classworks/:classworkId/:submissionId/update",
-	loggedInVerify,
-	classroomController.update_classwork_submissioin_patch
+  "/:classId/classworks/:classworkId/:submissionId/update",
+  loggedInVerify,
+  classroomController.update_classwork_submissioin_patch
 ); /** update classwork @method PATCH @endpoint `api/classroom/:classId/classworks/:classId/:submissionId/update`*/
 
 module.exports = classroomRouter;
